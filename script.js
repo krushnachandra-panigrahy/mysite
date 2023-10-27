73cc11bb6cd92b635fae3936861294d5bb806535
panigrahyk <147497765+panigrahyk@users.noreply.github.com>
2023-10-27 12:05:54 +0530

    changecount

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-// Function to update the counter and store it in local storage
-function updateCounter() {
-    var counterElement = document.getElementById("counter");
-    var count = parseInt(counterElement.textContent) || 0;
-    count++;
+const count=document.getElementById("count");
 
-    // Update the counter element and store the count in local storage
-    counterElement.textContent = count;
-    localStorage.setItem("visitorCount", count);
+totalUpdate();
+
+function totalUpdate(){
+fetch("https://api.countapi.xyz/hit/swapnilsparsh/?amount=1")
+.then((res)=>res.json())
+.then((res)=>{
+count.innerHTML=res.value;
+val(count,0,res.value,1000);
+});
 }
 
-// Check if the counter value is in local storage and update the page
-if (localStorage.getItem("visitorCount")) {
-    var count = localStorage.getItem("visitorCount");
-    document.getElementById("counter").textContent = count;
+function val(obj,start,end,duration){
+let startingStamp=null;
+const step=(st)=>{
+if(!startStamp) startingStamp=st;
+const progress=Math.min((st-startingStamp)/duration,1);
+obj.innerHTML=Math.floor(progress*(end-start)+start);
+if(progress<1){
+window.requestAnimationFrame(step);
 }
-
-// Add an event listener to increment the counter when the page loads
-window.addEventListener("load", updateCounter);
-
-// Initialize the counter to 1 by default
-var count = 1;
-
-// Check if the counter value is in local storage
-if (localStorage.getItem("visitorCount")) {
-    count = parseInt(localStorage.getItem("visitorCount"));
+};
+window.requestAnimationFrame(step);
 }
-
-// Increment the counter and display it
-var counterElement = document.getElementById("counter");
-var startCount = 1; // Start the animation from 1
-var endCount = count;
-
-function updateCounterAnimation() {
-    if (startCount <= endCount) {
-        counterElement.textContent = startCount;
-        startCount++;
-        counterElement.classList.add("counting-animation");
-        setTimeout(function () {
-            counterElement.classList.remove("counting-animation");
-            updateCounterAnimation(); // Call the function recursively
-        }, 1);
-    }
-}
-
-// Call the function to start the counting animation
-updateCounterAnimation();
